e331bcabb3c764fdbe397a069995a30100d1c9e9
chris_cy_wu <a092816190823@gmail.com>
2023-10-21 23:48:48 +0800

    加入附件檔案 Enitiy

---
+++ src/Domain/Attachment/Attachment.cs
@@ -0,0 +1,18 @@
+﻿namespace Domain.Attachment;
+
+public class Attachment
+{
+    public int Id { get; set; }
+
+    public string Type { get; set; }
+
+    public string OriFileName { get; set; }
+
+    public string FileName { get; set; }
+
+    public string Path { get; set; }
+
+    public DateTimeOffset CreatedAt { get; set; }
+
+    public string? CreatedBy { get; set; }
+}
---
+++ src/Infrastructure/EntityConfigurations/AttachmentConfiguration.cs
@@ -0,0 +1,42 @@
+﻿using Domain.Attachment;
+using Infrastructure.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.EntityConfigurations;
+
+public class AttachmentConfiguration : IEntityTypeConfiguration<Attachment>
+{
+    public void Configure(EntityTypeBuilder<Attachment> attachmentConfiguartion)
+    {
+        attachmentConfiguartion.ToTable("Attachment");
+
+        attachmentConfiguartion.HasKey(p => p.Id);
+
+        attachmentConfiguartion.Property(p => p.Type)
+            .HasMaxLength(256);
+
+        attachmentConfiguartion.HasDiscriminator<string>("Type")
+            .HasValue<Attachment>("Attachment");
+
+        attachmentConfiguartion.Property(p => p.OriFileName)
+            .HasMaxLength(256)
+            .IsRequired();
+
+        attachmentConfiguartion.Property(p => p.FileName)
+            .HasMaxLength(256)
+            .IsRequired();
+
+        attachmentConfiguartion.Property(p => p.Path)
+            .HasMaxLength(1024)
+            .IsRequired();
+
+        attachmentConfiguartion.Property(p => p.CreatedAt)
+            .IsRequired();
+
+        attachmentConfiguartion.HasOne<ApplicationUser>()
+            .WithMany()
+            .HasForeignKey(p => p.CreatedBy)
+            .OnDelete(DeleteBehavior.SetNull);
+    }
+}
