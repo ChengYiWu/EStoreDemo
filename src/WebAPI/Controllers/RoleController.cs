7176164f45bf295b3bfbfe9c753cf94b4b40ee6a
chris_cy_wu <a092816190823@gmail.com>
2023-11-06 11:27:08 +0800

    新增 [ 取得角色清單 ] API

---
+++ src/WebAPI/Controllers/RoleController.cs
@@ -0,0 +1,28 @@
+﻿using Application.Roles.Queries.GetRoleList;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/roles")]
+    [ApiController]
+    public class RoleController : ControllerBase
+    {
+        private readonly ISender _sender;
+
+        public RoleController(ISender sender)
+        {
+            _sender = sender;
+        }
+
+        [Authorize]
+        [HttpGet]
+        [Route("list")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<RoleListResponse> GetRoleListAsync()
+        {
+            return await _sender.Send(new GetRoleListQuery());
+        }
+    }
+}
