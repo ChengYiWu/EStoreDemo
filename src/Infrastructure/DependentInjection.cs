ed8873537805caf3209ff5e560af08c1d99b17f8
chris_cy_wu <a092816190823@gmail.com>
2023-11-21 17:38:58 +0800

    移除 retry 機制

--- src/Infrastructure/DependentInjection.cs
+++ src/Infrastructure/DependentInjection.cs
@@ -29,10 +29,7 @@ public static class DependencyInjection
         // DbContext
         var connectionStringOptions = configuration.GetSection("ConnectionStrings").Get<ConnectionStringOption>();
         services.AddDbContext<EStoreContext>(
-            c => c.UseSqlServer(connectionStringOptions.DefaultConnection, options =>
-            {
-                options.EnableRetryOnFailure(maxRetryCount: 3, maxRetryDelay: TimeSpan.FromSeconds(15), errorNumbersToAdd: null);
-            }),
+            c => c.UseSqlServer(connectionStringOptions.DefaultConnection),
             ServiceLifetime.Scoped
         );
 
